60e0dabf641da19d201a0dc8d898b017edea9307
Laurentino Gomes da Costa Neto <lauren_alfa@hotmail.com>
2025-05-04 15:26:02 -0400

    Delete carousel.js

--- carousel.js
+++
@@ -1,5 +0,0 @@
-document.querySelectorAll('.carousel-image').forEach(img => {
-    img.addEventListener('click', () => {
-        img.scrollIntoView({ behavior: 'smooth', inline: 'center' });
-    });
-});
